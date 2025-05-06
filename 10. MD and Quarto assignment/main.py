eb718bc54e946d970f915ed5e780e382dc796e11
Danfeng Zhang <convercraft@gmail.com>
2025-05-06 16:10:58 +0200

    using python script and jupyter to generate pdf by season in batch
    Signed-off-by: Danfeng Zhang <convercraft@gmail.com>

---
+++ 10. MD and Quarto assignment/main.py
@@ -0,0 +1,25 @@
+import subprocess
+import sys
+def generate_quarto_report(number: int):
+    filename = "season_"+str(number)+".pdf"
+    command = [
+        "quarto", "render",
+        "Assignment.qmd",
+        "--to", "pdf",
+        "--output", filename,
+        f"-P", f"season:{number}"
+    ]
+    subprocess.run(command, check=True)
+    print("✅ Generated filename")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <season_number>")
+        print("Usage: python main.py <season_number_fromer> <season_number_to>")
+        sys.exit(1)
+    if len(sys.argv) == 3:
+        for i in range(int(sys.argv[1]), int(sys.argv[2])+1):
+            generate_quarto_report(i)
+        sys.exit(0)
+    if len(sys.argv) ==2 :
+        generate_quarto_report(sys.argv[1])
